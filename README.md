4d1bfebc2ae2b530a2e9aee0189662443afae101
LuoHang <luohang@rongkecloud.com>
2016-08-15 19:08:30 +0800

    融科通JAVA版本V3.0更新
    1：增加用户多终端登录功能
    2：增加用户多终端消息同步功能

---
+++ README.md
@@ -0,0 +1,65 @@
+# RongKeMessenger-Server
+RongKeMessenger for Server（融科通Server端源码）
+
+![云视互动](http://www.rongkecloud.com/skin/simple/img/logo-small.png)
+
+[Home Page(官方主页)](http://www.rongkecloud.com) | [Doc(文档手册)](http://www.rongkecloud.com/download/rongketong/doc.zip) |  [CHANGELOG(更新历史)](https://github.com/rongkecloud/RongKeMessenger-Server/blob/master/CHANGELOG.md)
+
+## 功能介绍
+  融科通是基于云视互动打造的前、后完全开源APP，除完善的APP框架和后台设计外，还涵盖了注册、登录、通讯录管理、单聊、群聊、音视频通话、多人语音聊天室等即时通讯互动功能，陆续融科通还会逐步推出朋友圈、附近的人、多媒体客服等高级功能，旨在帮助广大开发者能够基于融科通开源代码上最低成本快速实现自身的产品。<br/>
+融科通下载支持的电子市场有：豌豆荚、360手机助手、应用宝、百度手机助手、91助手、安卓市场。
+
+## 基于开源框架融科通开发Server说明：
+
+    根据您的需要下载Server端Java版本、PHP版本融科通开源代码，使用Eclipse开发工具打开。
+
+### 代码配置说明 <br>
+#### 1、导入sql(MySql数据库)
+    创建融科通使用的数据库，导入融科通.sql，创建对应用户；
+#### 2、注册开发者账号<br/>
+    去云视互动官网注册开发者账号，登录后创建应用，获得客户端及服务端密钥。<br>
+    注册开发者流程文档,请访问：http://www.rongkecloud.com/document/<br>
+    开发者中心登录请访问：http://developer.rongkecloud.com/<br>
+#### 1、修改配置文件<br/>
+
+##### PHP代码配置文件修改<br>
+    修改config包下的rkdemo/sysconfig/sysConfig.ini文件中的数据库信息为用户自己的数据库信息<br>
+    MASTER_HOST=127.0.0.1   //DB服务器 ip地址<br>
+    MASTER_USER=username   //DB用户<br>
+    MASTER_PASS=password    //DB访问密码<br>
+    MASTER_NAME=rkcloud_test //DB名称<br>
+    MASTER_CHARSET=utf8<br>
+
+##### Java代码配置文件修改<br>
+    修改WEB-INF/config/servlet.conf配置文件，需要修改的部分：<br>
+    mysql.base.info = db_ip:3306,db_name,db_user,db_pwd<br>
+    db_ip替换为需要使用的数据库ip地址<br>
+    db_name替换为该应用所使用的数据库名称<br>
+    db_user 替换为连接数据库的用户名<br>
+    db_pwd 替换为连接数据库的密码<br>
+    应用在云视互动平台中的”服务器端密钥”，通过在云视互动官网注册开发者账号，创建应用后获得<br>
+    rkcloudapi_serverkey= 您注册的serverkey<br>
+    图片上传临时目录，配置后，请给予可读可写权限。<br>
+    如果遇见头像上传失败，请确认改目录是否配置以及是否有读写权限。<br>
+    image_temp=/upload<br>
+
+#### 2、修改融科通服务端秘钥 <br>
+##### PHP代码修改
+    修改RkCloudApi包下的RkCloud_Config.ini文件中的rkcloudapi_serverkey为您在开发者中心中创建应用的服务器密钥。<br>
+
+#### 3、代码部署<br>
+##### PHP代码部署<br>
+    将PHP版本的源码rkdemo解压到HTTP根目录，保证rkdemo目录及文件访问权限。<br>
+    在浏览器访问：http://your-server-ip/rkdemo/login.php<br>
+    返回:ret_code=9999，说明api部署成功。<br>
+
+##### Java代码部署<br>
+    将rkdemo项目生成.war文件，将此文件部署到tomcat服务器的webapps目录下，保证rkdemo目录及文件访问权限。<br>
+    在浏览器访问：http://your-server-ip:8080/rkdemo/login.php<br>
+    返回:ret_code=9999，说明api部署成功。<br>
+
+[Service Agreement(云视互动开发者平台服务协议)](http://www.rongkecloud.com/tecinfo/28.html)
+
+[Contact us(联系我们)][serviceLink]
+
+[serviceLink]: http://kefu.rongkecloud.com/RKServiceClientWeb/index.html?ek=6f2683bb7f9b98aa09283fd8b47f4086aec37b56&ct=1&bg=3&gd=143 "在线客服"
---
+++ 融科通.sql
@@ -0,0 +1,145 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.5.45 : Database - rkcloud_test
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`rkcloud_test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `rkcloud_test`;
+
+/*Table structure for table `feedbacks` */
+
+DROP TABLE IF EXISTS `feedbacks`;
+
+CREATE TABLE `feedbacks` (
+  `type` int(11) NOT NULL DEFAULT '0' COMMENT '问题类型id：1.软件问题，2.界面问题，3.其他问题',
+  `user_account` varchar(50) NOT NULL DEFAULT '' COMMENT '账号名称',
+  `content` varchar(500) CHARACTER SET utf8mb4 NOT NULL DEFAULT '' COMMENT '意见反馈内容',
+  `created` datetime NOT NULL COMMENT '创建时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `updateinfo` */
+
+DROP TABLE IF EXISTS `updateinfo`;
+
+CREATE TABLE `updateinfo` (
+  `os_type` varchar(50) NOT NULL DEFAULT '' COMMENT '操作系统类型，如：android、iphone',
+  `download_url` varchar(100) DEFAULT NULL COMMENT '下载地址',
+  `update_version` varchar(10) NOT NULL DEFAULT '' COMMENT '升级版本号',
+  `update_description` varchar(255) DEFAULT NULL COMMENT '升级内容描述',
+  `upload_date` datetime DEFAULT NULL COMMENT '升级日期',
+  `min_version` varchar(10) DEFAULT NULL COMMENT '最低版本号要求',
+  `file_name` varchar(100) DEFAULT NULL COMMENT '文件名称',
+  `file_size` int(10) unsigned DEFAULT NULL COMMENT '文件大小，单位：字节',
+  PRIMARY KEY (`os_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='版本更新表';
+
+/*Table structure for table `user_accounts` */
+
+DROP TABLE IF EXISTS `user_accounts`;
+
+CREATE TABLE `user_accounts` (
+  `user_account` varchar(20) NOT NULL DEFAULT '' COMMENT '账号名称',
+  `user_pwd` varchar(50) NOT NULL DEFAULT '' COMMENT '密码',
+  `sdk_pwd` varchar(50) NOT NULL COMMENT '云视互动登录密码',
+  `session` varchar(50) DEFAULT NULL COMMENT '登录session',
+  `user_type` int(11) NOT NULL DEFAULT '1' COMMENT '用户类型 1:normal 2:enterprise',
+  `name` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '姓名',
+  `sex` int(11) DEFAULT '0' COMMENT '性别 1:男 2：女',
+  `address` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '地址',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `mobile` varchar(20) DEFAULT NULL COMMENT '手机号码',
+  `permission_validation` int(11) DEFAULT '1' COMMENT '加我为好友的权限验证：1.需要验证，2.不需要验证',
+  `info_version` int(11) DEFAULT '0' COMMENT '信息版本号',
+  `avatar_version` int(11) DEFAULT '0' COMMENT '头像版本号',
+  `created` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`user_account`),
+  UNIQUE KEY `session` (`session`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `user_avatars` */
+
+DROP TABLE IF EXISTS `user_avatars`;
+
+CREATE TABLE `user_avatars` (
+  `user_account` varchar(50) NOT NULL DEFAULT '' COMMENT '账号名称',
+  `user_avatar` mediumblob COMMENT '头像',
+  `user_avatar_thumb` blob COMMENT '头像缩略图',
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_account`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `user_friends` */
+
+DROP TABLE IF EXISTS `user_friends`;
+
+CREATE TABLE `user_friends` (
+  `user_account` varchar(50) NOT NULL DEFAULT '' COMMENT '账号名称',
+  `friend_account` varchar(50) NOT NULL DEFAULT '' COMMENT '好友账号名称',
+  `gid` int(11) NOT NULL DEFAULT '0' COMMENT '组id',
+  `friend_remark` varchar(50) CHARACTER SET utf8mb4 NOT NULL DEFAULT '' COMMENT '好友备注',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_account`,`friend_account`),
+  KEY `gid` (`user_account`,`gid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `user_groups` */
+
+DROP TABLE IF EXISTS `user_groups`;
+
+CREATE TABLE `user_groups` (
+  `gid` int(11) NOT NULL AUTO_INCREMENT COMMENT '分组id',
+  `user_account` varchar(50) NOT NULL DEFAULT '' COMMENT '账号名称',
+  `group_name` varchar(50) CHARACTER SET utf8mb4 NOT NULL DEFAULT '' COMMENT '组名称',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`gid`),
+  KEY `user_account` (`user_account`)
+) ENGINE=InnoDB AUTO_INCREMENT=467 DEFAULT CHARSET=utf8;
+
+/* Trigger structure for table `user_avatars` */
+
+DELIMITER $$
+
+USE `rkcloud_test`$$
+
+DROP TRIGGER /*!50032 IF EXISTS */ `trg_user_avatar_insert`$$
+CREATE
+    TRIGGER `trg_user_avatar_insert` AFTER INSERT ON `user_avatars` 
+    FOR EACH ROW BEGIN
+	UPDATE user_accounts SET avatar_version=avatar_version+1 WHERE user_account = new.user_account;
+    END;
+$$
+DELIMITER ;
+
+/* Trigger structure for table `user_avatars` */
+
+DELIMITER $$
+
+USE `rkcloud_test`$$
+
+DROP TRIGGER /*!50032 IF EXISTS */ `trg_user_avatar_update`$$
+
+CREATE
+    TRIGGER `trg_user_avatar_update` AFTER UPDATE ON `user_avatars` 
+    FOR EACH ROW BEGIN
+	UPDATE user_accounts SET avatar_version=avatar_version+1 WHERE user_account = old.user_account;
+    END;
+$$
+DELIMITER ;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
